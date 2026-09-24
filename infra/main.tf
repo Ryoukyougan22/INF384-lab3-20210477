a60d9faf5c15246e90b7caf0c1ff708c5ed918f0
Ryoukyougan22 <a20210477@pucp.edu.pe>
2026-09-24 18:05:58 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -64,9 +64,8 @@ resource "aws_ecr_repository_policy" "descarga_lambda" {
 # El nombre del grupo debe coincidir con el que Lambda usa por convencion,
 # o la funcion crea el suyo y esta retencion no aplica.
 resource "aws_cloudwatch_log_group" "funcion" {
-  count = 0
 
-  name              = "/aws/lambda/${var.nombre_aplicacion}"
+  name              = "/aws/lambda/${var.nombre_aplicacion}-prueba-destruccion"
   retention_in_days = 7
 
 resource "aws_lambda_function" "app" {
